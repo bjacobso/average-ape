2f0c1f4e773903c13743e2878d5b2d5de21d4e5e
Ben Jacobson <ben.jacobson@indigov.us>
2021-12-30 12:08:21 -0800

    Add detail page

--- pages/[feature]/[name].tsx
+++ pages/[feature]/[name].tsx
@@ -1,12 +1,12 @@
 import type {NextPage} from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import apes from '../apes.json'
+import apes from '../../apes.json'
 import {useRouter} from "next/router";
 
 function dynamicSort(property: string) {
     let sortOrder = 1;
-    if(property[0] === "-") {
+    if (property[0] === "-") {
         sortOrder = -1;
         property = property.substr(1);
     }
@@ -19,6 +19,7 @@ function dynamicSort(property: string) {
 
 const Home: NextPage = () => {
     const router = useRouter()
+    const feature = apes.find((x) => x.type == router.query.feature)?.features.find((x) => x.feature == router.query.name)
 
     return (
         <div>
@@ -30,6 +31,29 @@ const Home: NextPage = () => {
             <h1 className={"text-2xl font-extrabold p-3 text-white bg-black"}>The Average Ape</h1>
 
             <main className={'container mx-auto'}>
+
+                {feature ? (
+                    <div className={'m-8'}>
+                        <div key={feature.feature} className={'flex flex-col justify-items-center items-center'}>
+                            <div
+                                className={'bg-gray-200 border-2 shadow-inner border-gray-500 overflow-hidden hover:border-purple-500 hover:bg-purple-200 rounded-lg'}
+                                style={{width: '631px', height: '631px', borderRadius: '50px'}}>
+                                <Image src={`/apes/${feature.filename}.png`}
+                                       alt={feature.feature}
+                                       width={'631px'}
+                                       height={'631px'}/>
+                            </div>
+                            <h3 className={'text-center text-xl text-bold text-gray-800 font-extrabold mt-5 mb-2'}>{router.query.feature} / {feature.feature}</h3>
+                            <h3 className={'text-center text-lg text-bold text-gray-500 mt-3 mb-3'}>{feature.count} total
+                                ({feature.count / 10_000}%)</h3>
+                        </div>
+                    </div>
+                ) : (
+                    <div className={'text-xl font-extrabold text-white bg-red-600 p-4 my-5'}>Not found...</div>
+                )}
+
+                <hr className={'mb-6'}/>
+
                 {
                     apes.sort(dynamicSort("type")).map((k) => (
                         <div key={k.type} className={'my-3'}>
@@ -39,7 +63,9 @@ const Home: NextPage = () => {
                             <div className={'grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-0 my-3'}>
                                 {
                                     k.features.sort(dynamicSort("count")).reverse().map((v) => (
-                                        <div key={v.feature} className={'flex flex-col justify-items-center items-center cursor-pointer'} onClick={() => router.push(`/${k.type}/${v.feature}`)}>
+                                        <div key={v.feature}
+                                             className={'flex flex-col justify-items-center items-center cursor-pointer'}
+                                             onClick={() => router.push(`/${k.type}/${v.feature}`)}>
                                             <div
                                                 className={'w-44 h-44 bg-gray-200 border hover:border-2 shadow-inner border-gray-500 hover:border-gray-500 hover:bg-purple-200 rounded-lg hover:drop-shadow-lg'}>
                                                 <Image src={`/apes/${v.filename}.png`}
